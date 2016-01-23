782c968c6ba6721575ef15ea35ac40dcd4caf97f
Nikolay Lebedev <nklbdev@gmail.com>
2016-01-23 15:43:09 +0500

    Project generated

---
+++ android/src/com/polly5315/slidingsquares/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.polly5315.slidingsquares.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.polly5315.slidingsquares.SlidingSquaresGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SlidingSquaresGame(), config);
+	}
+}
---
+++ desktop/src/com/polly5315/slidingsquares/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.polly5315.slidingsquares.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.polly5315.slidingsquares.SlidingSquaresGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlidingSquaresGame(), config);
+	}
+}
